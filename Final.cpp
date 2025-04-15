1890c76aa2b05ec93b8e0e0400675188aeb5f625
arafathossain2003169 <ah59642@gmail.com>
2025-04-15 15:30:23 +0600

    Full code

--- Final.cpp
+++ Final.cpp
@@ -1,7 +1,8 @@
-#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
 #include<gtest/gtest.h>
 
-// Create a C++ program that uses the Singleton design pattern to manage a single instance of a StringManager class, which holds a list of students.
+// Create a C++ program that uses the Singleton design pattern to manage a single instance of a StudentManager class, which holds a list of students.
 // Requirements:
 // Implement the Singleton design pattern.
 // Add functionalities to add, remove and display student names
@@ -9,3 +10,69 @@
 
 using namespace std;
 
+
+
+
+
+class StudentManager {
+private:
+    static StudentManager* instance;
+    vector<string> students;
+    StudentManager() {}
+
+public:
+    static StudentManager* getInstance() {
+        if (instance == nullptr) {
+            instance = new StudentManager();
+        }
+        return instance;
+    }
+
+    void addStudent(const string& name) {
+        students.push_back(name);
+    }
+
+    void removeStudent(const string& name) {
+        for (auto it = students.begin(); it != students.end(); ++it) {
+            if (*it == name) {
+                students.erase(it);
+                break;
+            }
+        }
+    }
+
+    void displayStudents() {
+        for (const auto& student : students) {
+            cout << student << endl;
+        }
+    }
+
+    vector<string> getStudents() {
+        return students;
+    }
+};
+
+StudentManager* StudentManager::instance = nullptr;
+
+TEST(StudentManagerTest, SingletonTest) {
+    StudentManager* manager1 = StudentManager::getInstance();
+    StudentManager* manager2 = StudentManager::getInstance();
+    ASSERT_EQ(manager1, manager2);
+}
+
+TEST(StudentManagerTest, AddRemoveTest) {
+    StudentManager* manager = StudentManager::getInstance();
+    manager->addStudent("John");
+    manager->addStudent("Alice");
+    auto students = manager->getStudents();
+    ASSERT_EQ(students.size(), 2);
+    manager->removeStudent("John");
+    students = manager->getStudents();
+    ASSERT_EQ(students.size(), 1);
+    ASSERT_EQ(students[0], "Alice");
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
