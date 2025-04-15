a4243745f3cc4a9b67531f64f5d35826e8e09542
arafathossain2003169 <ah59642@gmail.com>
2025-04-15 15:16:14 +0600

    test commit

---
+++ Final.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<gtest/gtest.h>
+
+// Create a C++ program that uses the Singleton design pattern to manage a single instance of a StringManager class, which holds a list of students.
+// Requirements:
+// Implement the Singleton design pattern.
+// Add functionalities to add, remove and display student names
+// Write at least 2 test cases using gtest
+
+using namespace std;
+
